154d3a074667e85bb60004288f0628d4e20ff9fa
vercingetorix47 <57217002+vercingetorix47@users.noreply.github.com>
2023-05-25 20:41:37 +0530

    Update Grib read main.py

--- Grib read main.py
+++ Grib read main.py
@@ -51,5 +51,5 @@ if __name__ == '__main__':
     test_object = wwst(gfswave_path,gdas_path)
     load_time_end = time.perf_counter()
     print(f"Loaded data in {load_time_end - load_time_start:0.4f} seconds")
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0')
     
