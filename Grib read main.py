840900af5292e92a85152e4135047f2003603fad
vercingetorix47 <57217002+vercingetorix47@users.noreply.github.com>
2023-06-08 14:04:38 +0530

    Update Grib read main.py

--- Grib read main.py
+++ Grib read main.py
@@ -4,52 +4,62 @@ from flask import Flask
 import json
  
 app = Flask(__name__)
-gfswave_path = r"./gfswave.t18z.global.0p16.f000.grib2"
-gdas_path = r"./gdas.t18z.pgrb2.1p00.f000"
+gfswave_path = [r"/climatedata/gfs0.grb",r"/climatedata/gfs1.grb",r"/climatedata/gfs2.grb",r"/climatedata/gfs3.grb",r"/climatedata/gfs4.grb",r"/climatedata/gfs5.grb"]
+gdas_path = [r"/climatedata/gdas0.grb",r"/climatedata/gdas1.grb",r"/climatedata/gdas2.grb",r"/climatedata/gdas3.grb",r"/climatedata/gdas4.grb",r"/climatedata/gdas5.grb"]
 
+     
 @app.get('/')
 def home():
-    return "Data already loaded.<br> Pass lat and lon as parameters to server address like so - <a href='http://127.0.0.1:5000/full/0/0'>http://127.0.0.1:5000/full/[lat]/[lon]</a> <br> Partial data retrieval can also be done in the following format with 0s or 1s for parameters other than lat or lon - <a href='http://127.0.0.1:5000/partial/0/0/1/1/1/1/1/1/1/1/1'>http://127.0.0.1:5000/partial/[lat]/[lon]/[wdir]/[ws]/[wvdir]/[wvht]/[wpd]/[swdir]/[swht]/[swpd]/[temp]</a>"
+    return "Data already loaded.<br> Pass lat and lon as parameters to server address like so - <a href='http://127.0.0.1:5000/single/0/0/0'>http://127.0.0.1:5000/single/[fhr]/[lat]/[lon]</a> <br> Partial data retrieval can also be done in the following format with 0s or 1s for parameters other than lat or lon - <a href='http://127.0.0.1:5000/partial/0/0/0/1/1/1/1/1/1/1/1/1'>http://127.0.0.1:5000/partial/[fhr]/[lat]/[lon]/[wdir]/[ws]/[wvdir]/[wvht]/[wpd]/[swdir]/[swht]/[swpd]/[temp]</a>"
 
-@app.get('/full/<lat>/<lon>')
-def full_payload(lat, lon):
+@app.get('/multi/<lat>/<lon>')
+def multigrib(lat,lon):
+     multi_payload = {}
+     for i in range (0,6):
+         multi_payload[i]=json.loads(test_object[i].return_json_payload(float(lat),float(lon)))
+     json_obj = json.dumps(multi_payload)
+     return json_obj
+ 
+@app.get('/single/<fhr>/<lat>/<lon>')
+def single_payload(fhr,lat, lon):
     retrieve_time_start = time.perf_counter()
-    json_obj = test_object.return_json_payload(float(lat),float(lon))
+    json_obj = test_object[fhr].return_json_payload(float(lat),float(lon))
     retrieve_time_end = time.perf_counter()
     print(f"Retrieved data in {retrieve_time_end - retrieve_time_start:0.4f} seconds")
     return json_obj
 
-@app.get('/partial/<lat>/<lon>/<wdir>/<ws>/<wvdir>/<shww>/<mpww>/<swdir>/<swell>/<swper>/<t>')
-def partial_payload(lat, lon, wdir=None, ws=None, wvdir=None, shww=None, mpww=None, swdir=None, swell=None, swper=None, t=None):
+@app.get('/partial/<fhr>/<lat>/<lon>/<wdir>/<ws>/<wvdir>/<shww>/<mpww>/<swdir>/<swell>/<swper>/<t>')
+def partial_payload(fhr,lat, lon, wdir=None, ws=None, wvdir=None, shww=None, mpww=None, swdir=None, swell=None, swper=None, t=None):
     dict = {}
     lat = float(lat)
     lon = float(lon)
     if wdir=='1':
-        dict['wind_direction']=str(test_object.wind_direction(lat, lon))
+        dict['wind_direction']=str(test_object[fhr].wind_direction(lat, lon))
     if ws=='1':
-        dict['wind_speed']=str(test_object.wind_speed(lat, lon))
+        dict['wind_speed']=str(test_object[fhr].wind_speed(lat, lon))
     if wvdir=='1':
-        dict['wave_direction']=str(test_object.wave_direction(lat, lon))
+        dict['wave_direction']=str(test_object[fhr].wave_direction(lat, lon))
     if shww=='1':
-        dict['wave_height']=str(test_object.wave_height(lat, lon))
+        dict['wave_height']=str(test_object[fhr].wave_height(lat, lon))
     if mpww=='1':
-        dict['wave_period']=str(test_object.wave_period(lat, lon))
+        dict['wave_period']=str(test_object[fhr].wave_period(lat, lon))
     if swdir=='1':
-        dict['swell_direction']=str(test_object.swell_direction(lat, lon))
+        dict['swell_direction']=str(test_object[fhr].swell_direction(lat, lon))
     if swell=='1':
-        dict['swell_height']=str(test_object.swell_height(lat, lon))
+        dict['swell_height']=str(test_object[fhr].swell_height(lat, lon))
     if swper=='1':
-        dict['swell_period']=str(test_object.swell_period(lat, lon))
+        dict['swell_period']=str(test_object[fhr].swell_period(lat, lon))
     if t=='1':
-        dict['air_temperature']=str(test_object.air_temperature(lat, lon))
+        dict['air_temperature']=str(test_object[fhr].air_temperature(lat, lon))
     json_obj = json.dumps(dict)
     return json_obj
  
 if __name__ == '__main__':
     load_time_start = time.perf_counter()
     global test_object
-    test_object = wwst(gfswave_path,gdas_path)
+    test_object=[]
+    for i in range(0,6):
+        test_object.append(wwst(gfswave_path[i],gdas_path[i]))
     load_time_end = time.perf_counter()
     print(f"Loaded data in {load_time_end - load_time_start:0.4f} seconds")
-    app.run(debug=True,host='0.0.0.0')
-    
+    app.run(debug=True)
